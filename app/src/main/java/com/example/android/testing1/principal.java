6e98974add7103fda415e40dbbf93618fb5845b0
nicoleescobar <niescobar.27@gmail.com>
2017-08-22 20:03:37 -0500

    interfaz op terminada

--- app/src/main/java/com/example/android/testing1/principal.java
+++ app/src/main/java/com/example/android/testing1/principal.java
@@ -4,13 +4,17 @@ import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class principal extends AppCompatActivity {
     private TextView res;
     private EditText n1, n2;
     private Resources resources;
+    private Spinner operaciones;
+    private String op[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,11 @@ public class principal extends AppCompatActivity {
         res = (TextView)findViewById(R.id.lblresultado);
         n1 = (EditText)findViewById(R.id.txtnum1);
         n2 = (EditText)findViewById(R.id.txtnum2);
+        operaciones = (Spinner)findViewById(R.id.cmbOperaciones);
         resources = this.getResources();
+        op = resources.getStringArray(R.array.operaciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, op);
+        operaciones.setAdapter(adapter);
     }
 
     public boolean validar(){
